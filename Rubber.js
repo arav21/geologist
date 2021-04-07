f67bd5fd284a15245f4ea5aec43c8ae684904e44
arav21 <77669267+arav21@users.noreply.github.com>
2021-04-07 16:07:05 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -1,6 +1,11 @@
 class Rubber{
 	constructor(x,y,r)
 	{
+		var options={
+			restitution:0.3,
+			friction:5,
+			density:1
+		}
 	// assign options to the rubber ball
 		this.x=x;
 		this.y=y;
@@ -19,7 +24,7 @@ class Rubber{
 			stroke("black");
 			fill("darkblue");
 			//draw the ellipse here to display the rubber ball
-
+			ellipse(0,0,this.r, this.r);
 			pop()
 	}
 
